7adbafb28663b4fc709d580ad0ba3cb5856d40e3
David Omer <davidomer@etalsolutions.tech>
2026-06-29 08:55:56 -0700

    Delete deploy/prd/portmason directory

--- deploy/prd/portmason/pm-setup.py
+++
@@ -1,6 +0,0 @@
-#!/home/davidomer/code/ops-and-sops/ops/portmason-py/.venv/bin/python
-import sys
-from portmason.commands.setup import main
-if __name__ == '__main__':
-    sys.argv[0] = sys.argv[0].removesuffix('.exe')
-    sys.exit(main())
